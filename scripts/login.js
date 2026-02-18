4c0fc83e1be60a5de3feadbbf5daa2c91956a64d
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-18 22:31:37 +0600

    add functionality to the login page using java script

---
+++ scripts/login.js
@@ -0,0 +1,30 @@
+const btns=document.getElementById("login-btn").addEventListener('click',function(){
+    //1: get the mobile number  input 
+     const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+
+    //2: get the mobile number pin
+    const inputPin = document.getElementById("input-pin");
+    const  pin = inputPin.value;
+    console.log(pin);
+   //3: match both mobile number and pin and make decision 
+   if(contactNumber == "01726236809" && pin == "1234"){
+         //3.1 if true :::>>>> alert> homepage 
+        // alert('Login successfully !!!');
+
+        //ay line dewar maane hole number and pin match hole se home page nia jaaabe ..  (replace use korle seita aager page er data te fire jete pare na but assign use korle login page to home page then abar home page e move korte paaare )
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+   }
+   else{
+    //3.2 if false :::>>>> alert> wrong information 
+    alert("Login failed !!!");
+    return;
+   }
+   
+    
+
+   
+
+});
